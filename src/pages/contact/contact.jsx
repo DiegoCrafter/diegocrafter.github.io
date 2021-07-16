d959ccb0755b15f63ab88c0fe5d6fd72a0217e74
DiegoCrafter <diegohuarcaya@upeu.edu.pe>
2021-07-16 15:19:56 -0500

    form working

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
 const Contact = () => {
+    const [show, setShow] = useState(false);
+
+    const validationSchema = Yup.object({
+        name: Yup.string().required(),
+        email: Yup.string().email().required(),
+        message: Yup.string().required()
+    })
+
+    const initialValues = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    const sendMesage = async (email, message, name) => {
+        axios.defaults.headers.post['Content-Type'] = 'application/json';
+        await axios.post('https://formsubmit.co/ajax/diegohuarcaya@upeu.edu.pe', {
+            name: name,
+            message: message,
+            email: email
+        }).then(response => {
+            if(response.status === 200){
+                setShow(true)
+            }
+        })
+            .catch(error => console.log(error))
+    }
+
+
     return (
         <div className="page pt-contact" data-simplebar>
             <section className="container">
@@ -10,41 +42,60 @@ const Contact = () => {
                     <span></span>
                 </div>
 
-                <div className="row mt-100">
-                    <div className="col-lg-12 col-sm-12">
-                        <div className="contact-form ">
-                            <form method="post" className="box-1 contact-valid" id="contact-form">
-                                <div className="row">
-                                    <div className="col-lg-6 col-sm-12">
-                                        <input type="text" name="name" id="name" className="form-control" placeholder="Name *" />
-                                    </div>
-                                    <div className="col-lg-6 col-sm-12">
-                                        <input type="email" name="email" id="email" className="form-control" placeholder="Email *" />
-                                    </div>
-                                    <div className="col-lg-12 col-sm-12">
-                                        <textarea className="form-control" name="note" id="note" placeholder="Your Message"></textarea>
-                                    </div>
-                                    <div className="col-lg-12 col-sm-12 text-center">
-                                        <button type="submit" className="btn-st">Send Message</button>
-                                        <div id="loader">
-                                            <i className="fas fa-sync"></i>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-12 col-sm-12">
-                                        <div className="error-messages">
-                                            <div id="success">
-                                                <i className="far fa-check-circle"></i>Thank you, your message has been sent.
+                <Formik
+                    validationSchema={validationSchema}
+                    initialValues={initialValues}
+                    onSubmit={(values, { resetForm }) => {
+                        const { email, message, name } = values;
+                        sendMesage(email, message, name)
+                        resetForm()
+                    }}
+                >
+                    {(formik) => {
+                        const { errors, touched } = formik;
+                        return (
+                            <div className="row mt-100">
+                                <div className="col-lg-12 col-sm-12">
+                                    <div className="contact-form ">
+                                        <Form method="post" className="box-1 contact-valid" id="contact-form">
+                                            <div className="row">
+                                                <div className="col-lg-6 col-sm-12">
+                                                    <Field type="text" name="name" id="name" className="form-control" placeholder="Name *" />
+                                                </div>
+                                                {touched.name && errors.name && <div>{errors.name}</div>}
+                                                <div className="col-lg-6 col-sm-12">
+                                                    <Field type="email" name="email" id="email" className="form-control" placeholder="Email *" />
+
+                                                </div>
+                                                {touched.email && errors.email && <div>{errors.email}</div>}
+                                                <div className="col-lg-12 col-sm-12">
+                                                    <Field className="form-control" name="message" id="note" placeholder="Your Message"></Field>
+                                                    {touched.message && errors.message && <div >{errors.message}</div>}
+                                                </div>
+
+                                                <div className="col-lg-12 col-sm-12 text-center mt-5">
+                                                    <button type="submit" className="btn-st">Send Message</button>
+                                                    <div id="loader">
+                                                        <i className="fas fa-sync"></i>
+                                                    </div>
+                                                </div>
+
                                             </div>
-                                            <div id="error">
-                                                <i className="far fa-times-circle"></i>Error occurred while sending email. Please try again later.
-                                            </div>
-                                        </div>
+                                        </Form>
                                     </div>
+                                    {
+                                        show ? <div className="alert alert-success mt-5" role="alert">
+                                            Thank you! your message was sent.
+                                        </div> : <div></div>
+                                    }
                                 </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+
+                            </div>
+
+                        )
+                    }}
+
+                </Formik>
 
 
                 <div className="box-2 contact-info">
